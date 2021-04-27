b50867f20aa53107e2834381d77af3886d0b7e63
Thomas <thomas.gagnier0@gmail.com>
2021-04-27 15:09:03 +0200

    Adding files developped

---
+++ menu_nav.php
@@ -0,0 +1,35 @@
+<!-- <?php
+if ($_SESSION["login"] == null) {
+?> -->
+<html>
+<header id="menu_nav">
+		<a href="#Accueil" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
+		<nav class="navbar">
+			<ul>
+				<li><a href="#Inscription" class="nav_button">Inscription</a></li>
+				<li><a href="#Connexion" class="nav_button">Connexion</a></li>
+				<li><a href="#FAQ" class="nav_button">FAQ</a></li>
+				<li><a href="#CGU" class="nav_button">CGU</a></li>
+			</ul>
+		</nav>
+		<script src="JSNav.js"></script>
+</header>
+</html>
+<!-- <?php } else { ?> -->
+<html>
+<header id="menu_nav">
+		<a href="#Accueil" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
+		<nav class="navbar">
+			<ul>
+				<li><a href="#Profil" class="nav_button">Profil</a></li>
+				<li><a href="#Données" class="nav_button">Mes données</a></li>
+				<li><a href="#FAQ" class="nav_button">FAQ</a></li>
+				<li><a href="#CGU" class="nav_button">CGU</a></li>
+			</ul>
+		</nav>
+		<script src="JSNav.js"></script>
+</header>
+</html>
+<!-- <?php } ?> -->
+
+
