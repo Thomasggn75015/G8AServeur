44aef5c6f3eaaf688d1f1e856d65c4a99286f1fe
Thomas <thomas.gagnier0@gmail.com>
2021-04-27 16:00:08 +0200

    Changing name to work on website

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php 
-echo ('Bonjour');
-?>
+<!DOCTYPE html>
+<html>
+	<head> 
+	        <meta charset="utf-8" />
+	        <link rel="stylesheet" href="style.css" />
+	        <title>Page d'accueil</title>
+	</head>
+
+	<header><?php include("menu_nav.php");?></header>
+		
+	<body class="body_accueil">
+		<div class="bloc_accueil">
+			<h1 class="titre_accueil">Bienvenue sur le site d'Infinite Measures</h1>
+
+			<h2 class="sous-titre_accueil">Améliorez vos performances à l'aide de nos tests de capacités physiques</h2>
+
+			<div class="bloc_photo">
+				<img class="photo_accueil" src="Images/sport.jpg" alt="photo de sports multiples" />
+
+				<div class="paragraphe_bouton">
+					<p class="paragraphe_accueil">
+						La société Infinite Measures réalise des tests de performance technique afin de vous aider à améliorer vos résultats dans votre sport de prédilection. Utiliser Infinite Measures, c'est s'assurer une progression constante.
+					</p>
+
+					<a href="#Inscription" title="bouton_essai" class="nav_button"> Rejoindre le programme</a>
+				</div>
+
+			</div>
+	</body>
+</html>
+
