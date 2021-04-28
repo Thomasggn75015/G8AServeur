0973cd73f03c794b8479adbd9e95c81ed9066073
Thomas <thomas.gagnier0@gmail.com>
2021-04-28 17:39:26 +0200

    Changement nom ancres

--- menu_nav.php
+++ menu_nav.php
@@ -3,19 +3,21 @@ if ($_SESSION["login"] == null) {
 ?> -->
 <html>
 <header id="menu_nav">
-		<a href="#Accueil" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
+		<a href="#index.php" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
 		<nav class="navbar">
 			<ul>
-				<li><a href="#Inscription" class="nav_button">Inscription</a></li>
-				<li><a href="#Connexion" class="nav_button">Connexion</a></li>
-				<li><a href="#FAQ" class="nav_button">FAQ</a></li>
-				<li><a href="#CGU" class="nav_button">CGU</a></li>
+				<li><a href="#page_enregistrement_sportif.php" class="nav_button">Inscription</a></li>
+				<li><a href="#connexion.php" class="nav_button">Connexion</a></li>
+				<li><a href="#faq.php" class="nav_button">FAQ</a></li>
+				<li><a href="#cgu.php" class="nav_button">CGU</a></li>
 			</ul>
 		</nav>
 		<script src="JSNav.js"></script>
 </header>
 </html>
+
 <!-- <?php } else { ?> -->
+
 <html>
 <header id="menu_nav">
 		<a href="#Accueil" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
