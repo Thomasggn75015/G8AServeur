e32fab6c8d987127c16db8cf938de5bc5a40f4b0
Ion Panteleiciuc <ipanteleiciuc@juniorisep.com>
2021-04-30 00:17:40 +0200

    Bug

--- pageConnexion.php
+++ pageConnexion.php
@@ -17,13 +17,58 @@
             <input type="text" placeholder="Login" name="username" class="Connexion_input_login"><br>
 
             <label>Password<br></label>
-            <input type="password" placeholder="Password" name="password" class="Connexion_input_password"><br>
+            <input type="text" placeholder="Password" name="password" class="Connexion_input_password"><br>
 
             <input type="submit" name="submitConnexion" value="Continuer" class="Connexion_bouton_continuer">
             <input type="submit" name="submitInscription" value="Inscription" class="Connexion_bouton_inscription">
         </form>
 
+        <?php 
+        if( ( isset($_POST['username']) ) && ($_POST['username'] != '') && ( isset($_POST['password']) ) && ($_POST['password'] != '')  
+        ){
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
+            }
+            catch (Exception $e)
+            {
+                    die('Erreur : ' . $e->getMessage());
+            }
 
+            $password = $_POST['password'];
+            $username = $_POST['username'];
+
+            echo gettype($password);
+            //echo $username;
+            
+
+            //$reponse = $bdd->query('SELECT mdp FROM CIA WHERE user = \''.$username.'\' ');
+
+            $reponse = $bdd -> prepare('SELECT mdp FROM CIA WHERE user = ?');
+            $reponse -> execute( array($_POST['username']) ); 
+
+            echo gettype($reponse);
+
+            $rep = serialize($reponse);
+
+            //echo ' '.$reponse;
+            //echo '<pre>'.print_r($reponse,true).'</pre>';
+
+            $autorise = "<p style = 'color : green;'> Accès autorisé ! </p>";
+            $refuse = "<p style = 'color : red;'> Accès refusé ! </p>";
+            
+
+            if($rep == $password){
+                echo "Je suis vrai";
+                echo $autorise;
+            } else {
+                echo "Je suis faux";
+                echo $refuse;
+            }
+            
+            
+        }
+        ?>
 
 
 
