ca95d11bbc775b32fb2a1fa378eb9a3cab2601f7
Thomas <thomas.gagnier0@gmail.com>
2021-04-29 11:34:05 +0200

    Sortie du dossier des fichiers de viviane

---
+++ faq.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head> 
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style_viviane.css" />
+        <title>FAQ</title>
+</head>
+
+<header>
+	<?php include("menu_nav.php");?>
+</header>
+
+<body>
+<div id="corps_FAQ_CGU">
+	
+	<h1 id="faq_cgu-titrePage">FAQ</h1>
+
+	  <div id="faq_main">
+
+		  <details class="faq_main-details">
+		    <summary class="faq_main-question">L'inscription aux services proposés par Infinite Measures est-elle gratuite ?</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">L'inscription au site est gratuite, mais l'appareil de mesures psychotechniques est payant.</p>
+		    </div>
+		  </details>
+
+		    <details class="faq_main-details">
+		    <summary class="faq_main-question">Est-ce que mes données psychotechniques sont publiques ?</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">Non, nous vous assurons que vos données et résultats sont accessibles uniquement par vous et votre coatch.</p>
+		    </div>
+		  </details>
+
+		  <details class="faq_main-details">
+		    <summary class="faq_main-question">Question 3</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">Réponse 3</p>
+		    </div>
+		  </details>
+
+		  <details class="faq_main-details">
+		    <summary class="faq_main-question">Question 4</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">Réponse 4</p>
+		    </div>
+		  </details>
+
+		  <details class="faq_main-details">
+		    <summary class="faq_main-question">Question 5</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">Réponse 5</p>
+		    </div>
+		  </details>
+
+		  <details class="faq_main-details">
+		    <summary class="faq_main-question">Question 6</summary>
+		    <div class="faq_main_contenu">
+		      <p class="faq_main_reponse">Réponse 6</p>
+		    </div>
+		  </details>
+
+	  </div>
+
+</div>
+</body>
+
+</html>
