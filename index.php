7d985ee3d0859750e50bc7b5e37822ca5af38f2b
Ion Panteleiciuc <ipanteleiciuc@juniorisep.com>
2021-04-29 10:55:40 +0200

    Commit avant PHP

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="UTF-8" >
-        <title>Documentation</title>
-
+  <head>
+        <title>Page de connexion</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="connexion.css" media="screen" type="text/css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">         
     </head>
 
     <body>
         <div id="bloc">
                 
-                <form action="" method="POST">
-                    <h1>Connexion</h1>
-                    
-                    <label><b>Nom d'utilisateur</b></label>
-                    
-                    <input type="text" placeholder="Utilisateur" name="username" ><br>
+        <div id = "container">
+            <form action="" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="email" placeholder="Entrer le login" name="username" required>
 
-                    <label><b>Mot de passe</b></label>
-                    <input type="password" placeholder="Mot de passe" name="password" ><br>
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                    <input type="submit" id='submit' value='LOGIN' >
-                </form>  
+                <input type="submit" id='submit' value='LOGIN' >
+                <!--<p>Si vous ne possédez pas de compte : <a href="connexion.html">Inscrivez-vous</a></p>-->
+                <input type="submit" id='submit2' value='INSCRIPTION' >
+            </form>
+
+        </div>  
 
                     <?php
 
