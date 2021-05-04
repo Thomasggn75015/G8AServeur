b410da4656df35ce7b87a6c97e1827c54623dc61
Max Gacoin <max.gacoin@sfr.fr>
2021-05-04 19:26:07 +0200

    integration coach et sportif dans un meme fichier et une meme table

--- page_enregistrement.php
+++ page_enregistrement.php
@@ -3,7 +3,8 @@
     <head>
         <title>Notre première instruction : echo</title>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="style_enregistrement_sportif.css" />
+        <link rel="stylesheet" href="style.css" />
+        <script src = 'script_enregistrement.js'></script>
     </head>
 
     <header>
@@ -30,12 +31,13 @@
             $tableUne = "CREATE Table IF NOT EXISTS Adherents(
                 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 firstname VARCHAR(30) NOT NULL,
-                lastename VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
                 coach VARCHAR(30) NOT NULL,
                 pseudo VARCHAR(30) NOT NULL,
-                mdp VARCHAR(30) NOT NULL,
+                mdp VARCHAR(255) NOT NULL,
                 email VARCHAR(50),
-                bithdate VARCHAR(20) NOT NULL
+                bithdate VARCHAR(20) NOT NULL,
+                role_user VARCHAR(20) NOT NULL
                 )";
 
             //Vérification de la connexion avec la table des adhérents
@@ -52,42 +54,54 @@
             //Données en INPUT renseignées par le nouvel adhérents et correspondants aux critères de la table des adhérents.
         ?>
             <div class = "enregistrement_formulaire">
-                <h1 class = "enregistrement"><B class = 'titreEnGras'>Créer un compte</B>(sportif)</h1>
-                <form class = 'form' ACTION = "page_enregistrement_sportif.php" METHOD = "POST">
+                <h1 class = "enregistrement"><B class = 'titreEnGras'>Créer un compte</h1>
+                <form class = 'form' NAME = "form" ACTION = "page_enregistrement.php" METHOD = "POST">
                     <label for = 'prenom'>Votre prénom</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'prenom', name = 'prenom' placeholder = 'Prénom' required minlength="2"></br>                                            <!--PRENOM-->
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <label for = 'nom'>Votre nom</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'nom', name = 'nom' placeholder = 'NOM' required minlength="2"></br>                                                     <!--NOM-->
                     <p class = 'interligneValider_enregistrement'></p>
+
+                    <label for = 'check_coach'>Avez-vous un entraineur ? </label>
+                    <INPUT TYPE = 'radio', id = 'check_coach_yes', name = 'checkcoach', onClick = "control_CB(form)"> OUI    
+                    <INPUT TYPE = 'radio', id = 'check_coach_no', name = 'checkcoach', onClick = "control_CB(form)"> NON    
+                    <INPUT TYPE = 'radio', id = 'check_coach_iam', name = 'checkcoach', onClick = "control_CB(form)"> JE SUIS ENTRAINEUR</br>
+
                     <label for = 'coach'>Nom de votre coach</label></br>
-                    <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'coach', name = 'coach' placeholder = 'COACH' required minlength = "2"></br>                                         <!--NOM DU COACH-->
+                    <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'coach', name = 'coach', value = "" placeholder = 'COACH' minlength = "2" disabled required>                                         <!--NOM DU COACH-->
+                    <INPUT TYPE = 'hidden', id = 'role_user'  NAME = "role_user", placeholder = "noValue" required>
+                    <INPUT TYPE = 'hidden', id = 'store_coach'  NAME = "store_coach", placeholder = "noValue" required>
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <label for = 'pseudo'>Votre pseudo</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'pseudo', name = 'pseudo' placeholder = "pseudo" required minlength="2"></br>                                            <!--PSEUDO-->
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <label for = 'email'>E-mail</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'EMAIL', id = 'email', name = 'email' placeholder = "e-mail" required minlength="2"></br>                                             <!--E-MAIL-->
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <label for = 'pswd'>Votre mot de passe</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'TEXT', id = 'pswd', name = 'pswd' placeholder = "Au moins 6 caractères" required minlength="8" maxlength="50"></br>                                 <!--MOT DE PASSE-->
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <label for = 'datedenaissance'>Votre date de naissance</label></br>
                     <INPUT class = "input_enregistrement" TYPE = 'NAME', id = 'datedenaissance', name = 'datedenaissance' placeholder = "jj/mm/aaaa" required minlength="2"></br>                      <!--DATE DE NAISSANCE-->
-
                     <p class = 'interligneValider_enregistrement'></p>
-                    <INPUT class = "boutonValider_enregistrement" TYPE = 'SUBMIT', VALUE = 'Créer votre compte Percutest'><br>
+
+                    <INPUT class = "boutonValider_enregistrement" TYPE = 'SUBMIT', VALUE = 'Créer votre compte Percutest', onClick = "definition_coach()"><br>
                     <p>En créant un compte, vous acceptez les <a href = "cgu.php">Conditions générales de vente</a> d'Infinite Mesure. Pour toute question, veuillez consulter notre <a href = "faq.php">FAQ</a></p>
                     <p class = 'interligneValider_enregistrement'></p>
+
                     <hr WIDTH = "300" ALIGN = CENTER>
                     <p class = 'interligneValider_enregistrement'></p>
-                    <p>Vous possédez déjà un compte ? <a href = "connexion.php">Identifiez-vous</a></br>
-                    Vous voulez vous incrire en tant qu'entraîneur sportif ? <a href = "page_enregistrement_coach.php">Créer votre compte<?php $AfficherFormulaire = 2?></a></p>
+                    <p>Vous possédez déjà un compte ? <a href = "connexion.php">Identifiez-vous</a></p>
                 </form>
-                </p>
 
                 <?php
-                if(isset($_POST['prenom'], $_POST['nom'], $_POST['coach'], $_POST['pseudo'], $_POST['pswd'], $_POST['email'], $_POST['datedenaissance'])){
+                if(isset($_POST['prenom'], $_POST['nom'], $_POST['store_coach'], $_POST['pseudo'], $_POST['pswd'], $_POST['email'], $_POST['datedenaissance'], $_POST['role_user'])){
                     $detection_erreur_enregistrement = 0;
                     $detail_erreur_enregistrement = "";
                     if(!preg_match("#^[a-zA-Z]+$#",$_POST['prenom'])){
@@ -101,7 +115,8 @@
                         $detail_erreur_enregistrement = "Le nom n'est pas valide / Le nom doit être en majuscule";
                     }
                     //Condition sur le NOM DU COACH
-                    elseif(!preg_match("#^[A-Z]+$#",$_POST['coach'])){
+                    elseif(!preg_match("#^[A-Z]+$#",$_POST['store_coach'])){
+                        echo $_POST['store_coach'];
                         $detection_erreur_enregistrement = 1;
                         $detail_erreur_enregistrement = "Le nom de votre coach n'est pas valide / Le nom de votre coach doit être en majuscule";
                     }
@@ -115,7 +130,7 @@
                         $detail_erreur_enregistrement = "Ce pseudo est déjà utilisé";
                     }
                     //Condition sur le MOT DE PASSE
-                    elseif(!preg_match("#^[a-z0-9]+$#",$_POST['pswd'])){
+                    elseif(!preg_match("#^[a-zA-Z0-9]+$#",$_POST['pswd'])){
                         $detection_erreur_enregistrement = 1;
                         $detail_erreur_enregistrement = "Le format du mot de passe n'est pas valable";
                     }
@@ -130,7 +145,8 @@
                         $detail_erreur_enregistrement = "Cette adresse e-mail est déjà utilisée";
                     }
                     else{
-                        $sql = "INSERT INTO Adherents SET firstname = '".$_POST['prenom']."', lastename = '".$_POST['nom']."', coach = '".$_POST['coach']."', pseudo = '".$_POST['pseudo']."', mdp = '".$_POST['pswd']."', email = '".$_POST['email']."', bithdate = '".$_POST['datedenaissance']."'";
+                        //echo $_POST['role_user'];
+                        $sql = "INSERT INTO Adherents SET firstname = '".$_POST['prenom']."', lastname = '".$_POST['nom']."', coach = '".$_POST['store_coach']."', pseudo = '".$_POST['pseudo']."', mdp = '".md5($_POST['pswd'])."', email = '".$_POST['email']."', bithdate = '".$_POST['datedenaissance']."', role_user = '".$_POST['role_user']."'";
                         if(!mysqli_query($conn, $sql)){
                             echo "Une erreur s'est produite: ".mysqli_error($conn);
                         } else{
