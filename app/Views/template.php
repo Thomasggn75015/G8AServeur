79598d5c45db3cec9e00fce5781cc3fbed47b0f4
Thomas <thomas.gagnier0@gmail.com>
2021-05-27 20:41:29 +0200

    MVC fonctionnel

--- app/Views/template.php
+++ app/Views/template.php
@@ -17,38 +17,50 @@ if(isset($_SESSION['user_role'])){
         <link href="public/css/style.css" rel="stylesheet" /> 
     </head>
 
-    <header id="menu_nav">
-        <?php 
-            if(isset($_SESSION['user_id'])){
-        ?> 
-                <a href="index.php" id="link_logo"><img id="logo" src="Images/Infinite_measures.png"  alt="logo Accueil" /></a>
-                <nav class="navbar">
-                    <ul>
-                        <li><a href="page_enregistrement.php" class="nav_button">Inscription</a></li>
-                        <li><a href="connexion.php" class="nav_button">Connexion</a></li>
-                        <li><a href="faq.php" class="nav_button">FAQ</a></li>
-                        <li><a href="cgu.php" class="nav_button">CGU</a></li>
-                    </ul>
-                </nav>
-            <?php
-            } else {
-            ?>
-                <a href="index.php" id="link_logo"><img id="logo" src="public/images/Infinite_measures.png"  alt="logo Accueil" /></a>
-                <nav class="navbar">
-                    <ul>
-                        <li><a href="Profil" class="nav_button">Profil</a></li>
-                        <li><a href="Données" class="nav_button">Mes données</a></li>
-                        <li><a href="faq.php" class="nav_button">FAQ</a></li>
-                        <li><a href="cgu.php" class="nav_button">CGU</a></li>
-                    </ul>
-                </nav>
-            <?php
-            }
-            ?>
-        <script src="public/js/JSNav.js"></script>
-    </header>
-
     <body>
-    <?= $content ?>
+        <header id="menu_nav">
+            <?php 
+                if(isset($_SESSION['user_id'])){
+            ?> 
+                    <a href="index.php" id="link_logo"><img id="logo" src="public/images/Infinite_measures.png"  alt="logo Accueil" /></a>
+                    <nav class="navbar">
+                        <ul>
+                            <li><a href="/profil" class="nav_button">Profil</a></li>
+                            <li><a href="/data_user" class="nav_button">Mes données</a></li>
+                            <li><a href="/faq" class="nav_button">FAQ</a></li>
+                        </ul>
+                    </nav>
+                <?php
+                    } else {
+                ?>
+                    <a href="index.php" id="link_logo"><img id="logo" src="public/images/Infinite_measures.png"  alt="logo Accueil" /></a>
+                    <nav class="navbar">
+                        <ul>
+                            <li><a href="/enregistrement" class="nav_button">Inscription</a></li>
+                            <li><a href="/connexion" class="nav_button">Connexion</a></li>
+                            <li><a href="/faq" class="nav_button">FAQ</a></li>
+                        </ul>
+                    </nav>
+                <?php
+                    }
+                ?>
+        </header>
+
+        <?= $content ?>
+
+        <footer class="footer">
+                <div class="footer_content">
+                    <ul class="social_logos">
+                        <li><a href="https://fr.linkedin.com" class="footer_link"><img src="public/images/linkedin.png" alt="logo linkedIn" class="social_logo"/></a></li> <!-- LinkedIn, facebook, Contact mail -->
+                        <li><a href="https://www.facebook.com" class="footer_link"><img src="public/images/facebook.png" alt="logo Facebook" class="social_logo"/></a></li>
+                        <li><a href="/sendMail" class="footer_link"><img src="public/images/mail.png" alt="logo Mail" class="social_logo"/></a></li>
+                    </ul>
+                    <h1 class="footer_contact-message"> Contactez nous </h1>
+                    <ul class="footer_nav">
+                        <li><a href="/cgu" class="footer_link">CGU</a></li>
+                        <li><a href="/legalMention" class="footer_link">Mentions légales</a></li>
+                    </ul>
+                <div>
+        </footer>
     </body>
 </html>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ define('VIEWS', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'G8AServeur/app/Views' 
 $router = new App\Router\Router($_GET['url']);
 
 //Lignes d'exemples :
-$router->get('/', "Post#homepage");
+$router->get('/', "Main#accueil");
 //On passe :slug et :id en paramètres, on appelle le contrôleur Post et sa méthode show, séparateur #
 $router->get('/posts/:slug-:id', "Post#show")->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
 
