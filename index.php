6196b63e14c3308ced258aa80de2e255960ad506
Thomas <thomas.gagnier0@gmail.com>
2021-05-28 09:16:50 +0200

    Add path pour enregistrement et cgu

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ define('VIEWS', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'G8AServeur/app/Views' 
 
 $router = new App\Router\Router($_GET['url']);
 
-//Lignes d'exemples :
-$router->get('/', "Main#accueil");
+//Redirection depuis l'accueil
+$router->get('/', 'Main#accueil');
+$router->get('/cgu', 'Main#cgu');
+$router->get('/enregistrement', 'Main#enregistrement');
+
+
 //On passe :slug et :id en paramètres, on appelle le contrôleur Post et sa méthode show, séparateur #
 $router->get('/posts/:slug-:id', "Post#show")->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
 
