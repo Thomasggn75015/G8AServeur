e9f366eea8820e44b5183924ab163ac1b5a7708a
Max Gacoin <max.gacoin@sfr.fr>
2021-06-01 23:42:08 +0200

    style profil change

--- app/Views/main/profil.php
+++ app/Views/main/profil.php
@@ -1,56 +1,4 @@
 <div class="bloc-profil">
-    <?php
-    if($_SESSION['user_role'] == 'coach' || $_SESSION['user_role'] == 'admin'){
-        echo ('
-        <form class="form-recherche" method="POST" action="/profil">
-            <label for="critereSelect"><h5>Selon quel critère souhaitez-vous rechercher un utilisateur ?</h5></label>
-            <select name="critereSelect" id="critereSelect">
-                <option value="pseudo">Nom d\'utilisateur</option>
-                <option value="email">Mail</option>
-                <option value="coachname">Nom du coach</option>
-                <option value="firstname">Prénom</option>
-                <option value="lastname">Nom</option>
-                <option value="birthdate">Date de naissance</option>
-            </select>
-            <label class="label-recherche"><h5>Entrer les critères de recherche d\'utilisateur</h5></label>
-            <input type="text" name="searchEntry" placeholder="ex. Jean"/>
-            <br/>
-            <button type="submit">Rechercher</button>
-        </form>
-        ');
-    }
-    if(isset($params)){
-        echo ('
-                <table class="tableau_resultats-recherche>
-                <colgroup span="7" class="columns"></colgroup>
-                    <tr>
-                        <th>Role</th>
-                        <th>Prénom</th>
-                        <th>Nom</th>
-                        <th>Nom du coach</th>
-                        <th>Nom d\'utilisateur</th>
-                        <th>Email</th>
-                        <th>Date de naissance</th>
-                    </tr>
-        ');
-            foreach($params as $row){
-                echo('
-                    <td> ' . $row["user_role"] . ' </td>
-                    <td> ' . $row["firstname"] . ' </td>
-                    <td> ' . $row["lastname"] . ' </td>
-                    <td> ' . $row["coachname"] . ' </td>
-                    <td> ' . $row["pseudo"] . ' </td>
-                    <td> ' . $row["email"] . ' </td>
-                    <td> ' . $row["birthdate"] . ' </td>
-                ');
-            }
-            echo('
-                        </tr>
-                    </tbody>
-                </table>
-            ');
-    }
-    ?>
     <div class="profil_form-bloc">
             <h1>Modification du profil</h1>
             <?php 
@@ -77,5 +25,59 @@
                     <br/>
                     <input type="submit" value="Valider les changements"/>
             </form>
+    </div>
+    <div id='recherche_utilisateur'>
+        <?php
+        if($_SESSION['user_role'] == 'coach' || $_SESSION['user_role'] == 'admin'){
+            echo ('
+            <form class="form-recherche" method="POST" action="/profil">
+                <label for="critereSelect"><h5>Selon quel critère souhaitez-vous rechercher un utilisateur ?</h5></label>
+                <select name="critereSelect" id="critereSelect">
+                    <option value="pseudo">Nom d\'utilisateur</option>
+                    <option value="email">Mail</option>
+                    <option value="coachname">Nom du coach</option>
+                    <option value="firstname">Prénom</option>
+                    <option value="lastname">Nom</option>
+                    <option value="birthdate">Date de naissance</option>
+                </select>
+                <label class="label-recherche"><h5>Entrer les critères de recherche d\'utilisateur</h5></label>
+                <input type="text" name="searchEntry" placeholder="ex. Jean"/>
+                <br/>
+                <button type="submit">Rechercher</button>
+            </form>
+            ');
+        }
+        if(isset($params)){
+            echo ('
+                    <table class="tableau_resultats-recherche>
+                    <colgroup span="7" class="columns"></colgroup>
+                        <tr>
+                            <th>Role</th>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Nom du coach</th>
+                            <th>Nom d\'utilisateur</th>
+                            <th>Email</th>
+                            <th>Date de naissance</th>
+                        </tr>
+            ');
+                foreach($params as $row){
+                    echo('
+                        <td> ' . $row["user_role"] . ' </td>
+                        <td> ' . $row["firstname"] . ' </td>
+                        <td> ' . $row["lastname"] . ' </td>
+                        <td> ' . $row["coachname"] . ' </td>
+                        <td> ' . $row["pseudo"] . ' </td>
+                        <td> ' . $row["email"] . ' </td>
+                        <td> ' . $row["birthdate"] . ' </td>
+                    ');
+                }
+                echo('
+                            </tr>
+                        </tbody>
+                    </table>
+                ');
+        }
+        ?>
     </div>
 </div>
